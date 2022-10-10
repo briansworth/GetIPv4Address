88c3a9e6d44c5dd212c63234af0012dde85434e5
Brian Farnsworth <brianfarnsworth@outlook.com>
2022-10-10 17:32:11 -0400

    Re-organize module

--- GetIPv4Subnet.psm1
+++ GetIPv4Subnet.psm1
@@ -24,44 +24,159 @@ function Convert-IPv4AddressToBinaryString
   return $strBuilder.ToString()
 }
 
-function ConvertIPv4ToInt
+function Get-IPv4Subnet
 {
-  [CmdletBinding()]
+  <#
+      .SYNOPSIS
+      Get information about an IPv4 subnet based on an IP Address and a subnet mask or prefix length
+
+      .DESCRIPTION
+      Get information about an IPv4 subnet based on an IP Address and a subnet mask or prefix length
+
+      .PARAMETER IPAddress
+      The IP Address to use for determining subnet information.
+
+      .PARAMETER PrefixLength
+      The prefix length of the subnet.
+
+      .PARAMETER SubnetMask
+      The subnet mask of the subnet.
+
+      .EXAMPLE
+      Get-IPv4Subnet -IPAddress 192.168.34.76 -SubnetMask 255.255.128.0
+
+      CidrID       : 192.168.0.0/17
+      NetworkID    : 192.168.0.0
+      SubnetMask   : 255.255.128.0
+      PrefixLength : 17
+      HostCount    : 32766
+      FirstHostIP  : 192.168.0.1
+      LastHostIP   : 192.168.127.254
+      Broadcast    : 192.168.127.255
+
+      Description
+      -----------
+      This command will get the subnet information about the IPAddress 192.168.34.76, with the subnet mask of 255.255.128.0
+
+      .EXAMPLE
+      Get-IPv4Subnet -IPAddress 10.3.40.54 -PrefixLength 25
+
+      CidrID       : 10.3.40.0/25
+      NetworkID    : 10.3.40.0
+      SubnetMask   : 255.255.255.128
+      PrefixLength : 25
+      HostCount    : 126
+      FirstHostIP  : 10.3.40.1
+      LastHostIP   : 10.3.40.126
+      Broadcast    : 10.3.40.127
+
+      Description
+      -----------
+      This command will get the subnet information about the IPAddress 10.3.40.54, with the subnet prefix length of 25.
+      Prefix length specifies the number of bits in the IP address that are to be used as the subnet mask.
+
+  #>
+  [CmdletBinding(DefaultParameterSetName = 'PrefixLength')]
   param(
-    [String]$IPv4Address
+    [Parameter(
+      Mandatory = $true,
+      ValueFromPipeline = $true,
+      ValueFromPipelineByPropertyName = $true,
+      ValueFromRemainingArguments = $false,
+      HelpMessage = 'IP Address in the form of XXX.XXX.XXX.XXX',
+      Position = 0
+    )]
+    [IPAddress]$IPAddress
+    ,
+    [Parameter(
+      Position = 1,
+      ParameterSetName = 'PrefixLength',
+      ValueFromPipeline = $true,
+      ValueFromPipelineByPropertyName = $true
+    )]
+    [Int16]$PrefixLength = 24
+    ,
+    [Parameter(Mandatory = $true, Position = 1, ParameterSetName = 'SubnetMask')]
+    [IPAddress]$SubnetMask
+    ,
+    [Parameter(
+      Mandatory = $true,
+      Position = 1,
+      ParameterSetName = 'Hosts',
+      HelpMessage = 'Number of hosts in need of IP Addresses'
+    )]
+    [Int64]$HostCount
   )
-  try
+  begin
   {
-    $ipAddress = [IPAddress]::Parse($IPv4Address)
-
-    $bytes = $IPAddress.GetAddressBytes()
-    [Array]::Reverse($bytes)
-
-    return [BitConverter]::ToUInt32($bytes, 0)
   }
-  catch
+  process
   {
-    Write-Error -Exception $_.Exception `
-      -Category $_.CategoryInfo.Category
-  }
-}
+    try
+    {
+      if ($PSCmdlet.ParameterSetName -eq 'SubnetMask')
+      {
+        $PrefixLength = Convert-NetMaskToCIDR -SubnetMask $SubnetMask `
+          -ErrorAction Stop
+      }
+      else
+      {
+        $SubnetMask = Convert-CIDRToNetMask -PrefixLength $PrefixLength `
+          -ErrorAction Stop
+      }
+      if ($PSCmdlet.ParameterSetName -eq 'Hosts')
+      {
+        $PrefixLength = (Get-CidrFromHostCount -HostCount $HostCount).PrefixLength
+        $SubnetMask = Convert-CIDRToNetMask -PrefixLength $PrefixLength `
+          -ErrorAction Stop
+      }
+      $maxHosts = [math]::Pow(2, (32 - $PrefixLength)) - 2
 
-function ConvertIntToIPv4
-{
-  [CmdletBinding()]
-  param(
-    [uint32]$Integer
-  )
-  try
-  {
-    $bytes = [BitConverter]::GetBytes($Integer)
-    [Array]::Reverse($bytes)
-    ([IPAddress]($bytes)).ToString()
+      $netMaskInt = ConvertIPv4ToInt -IPv4Address $SubnetMask
+      $ipInt = ConvertIPv4ToInt -IPv4Address $IPAddress
+
+      $networkID = ConvertIntToIPv4 -Integer ($netMaskInt -band $ipInt)
+
+      $broadcast = Add-IntToIPv4Address -IPv4Address $networkID `
+        -Integer ($maxHosts + 1)
+
+      $firstIP = Add-IntToIPv4Address -IPv4Address $networkID -Integer 1
+      $lastIP = Add-IntToIPv4Address -IPv4Address $broadcast -Integer (-1)
+
+      if ($PrefixLength -eq 32)
+      {
+        $broadcast = $networkID
+        $firstIP = $null
+        $lastIP = $null
+        $maxHosts = 0
+      }
+
+      $outputObject = New-Object -TypeName PSObject
+
+      $memberParam = @{
+        InputObject = $outputObject
+        MemberType  = 'NoteProperty'
+        Force       = $true
+      }
+      Add-Member @memberParam -Name CidrID -Value ('{0}/{1}' -f $networkID, $PrefixLength)
+      Add-Member @memberParam -Name NetworkID -Value $networkID
+      Add-Member @memberParam -Name SubnetMask -Value $SubnetMask
+      Add-Member @memberParam -Name PrefixLength -Value $PrefixLength
+      Add-Member @memberParam -Name HostCount -Value $maxHosts
+      Add-Member @memberParam -Name FirstHostIP -Value $firstIP
+      Add-Member @memberParam -Name LastHostIP -Value $lastIP
+      Add-Member @memberParam -Name Broadcast -Value $broadcast
+
+      Write-Output -InputObject $outputObject
+    }
+    catch
+    {
+      Write-Error -Exception $_.Exception `
+        -Category $_.CategoryInfo.Category
+    }
   }
-  catch
+  end
   {
-    Write-Error -Exception $_.Exception `
-      -Category $_.CategoryInfo.Category
   }
 }
 
@@ -246,159 +361,44 @@ function Get-CidrFromHostCount
   }
 }
 
-function Get-IPv4Subnet
+function ConvertIPv4ToInt
 {
-  <#
-      .SYNOPSIS
-      Get information about an IPv4 subnet based on an IP Address and a subnet mask or prefix length
-
-      .DESCRIPTION
-      Get information about an IPv4 subnet based on an IP Address and a subnet mask or prefix length
-
-      .PARAMETER IPAddress
-      The IP Address to use for determining subnet information.
-
-      .PARAMETER PrefixLength
-      The prefix length of the subnet.
-
-      .PARAMETER SubnetMask
-      The subnet mask of the subnet.
-
-      .EXAMPLE
-      Get-IPv4Subnet -IPAddress 192.168.34.76 -SubnetMask 255.255.128.0
-
-      CidrID       : 192.168.0.0/17
-      NetworkID    : 192.168.0.0
-      SubnetMask   : 255.255.128.0
-      PrefixLength : 17
-      HostCount    : 32766
-      FirstHostIP  : 192.168.0.1
-      LastHostIP   : 192.168.127.254
-      Broadcast    : 192.168.127.255
-
-      Description
-      -----------
-      This command will get the subnet information about the IPAddress 192.168.34.76, with the subnet mask of 255.255.128.0
-
-      .EXAMPLE
-      Get-IPv4Subnet -IPAddress 10.3.40.54 -PrefixLength 25
-
-      CidrID       : 10.3.40.0/25
-      NetworkID    : 10.3.40.0
-      SubnetMask   : 255.255.255.128
-      PrefixLength : 25
-      HostCount    : 126
-      FirstHostIP  : 10.3.40.1
-      LastHostIP   : 10.3.40.126
-      Broadcast    : 10.3.40.127
-
-      Description
-      -----------
-      This command will get the subnet information about the IPAddress 10.3.40.54, with the subnet prefix length of 25.
-      Prefix length specifies the number of bits in the IP address that are to be used as the subnet mask.
-
-  #>
-  [CmdletBinding(DefaultParameterSetName = 'PrefixLength')]
+  [CmdletBinding()]
   param(
-    [Parameter(
-      Mandatory = $true,
-      ValueFromPipeline = $true,
-      ValueFromPipelineByPropertyName = $true,
-      ValueFromRemainingArguments = $false,
-      HelpMessage = 'IP Address in the form of XXX.XXX.XXX.XXX',
-      Position = 0
-    )]
-    [IPAddress]$IPAddress
-    ,
-    [Parameter(
-      Position = 1,
-      ParameterSetName = 'PrefixLength',
-      ValueFromPipeline = $true,
-      ValueFromPipelineByPropertyName = $true
-    )]
-    [Int16]$PrefixLength = 24
-    ,
-    [Parameter(Mandatory = $true, Position = 1, ParameterSetName = 'SubnetMask')]
-    [IPAddress]$SubnetMask
-    ,
-    [Parameter(
-      Mandatory = $true,
-      Position = 1,
-      ParameterSetName = 'Hosts',
-      HelpMessage = 'Number of hosts in need of IP Addresses'
-    )]
-    [Int64]$HostCount
+    [String]$IPv4Address
   )
-  begin
+  try
   {
+    $ipAddress = [IPAddress]::Parse($IPv4Address)
+
+    $bytes = $IPAddress.GetAddressBytes()
+    [Array]::Reverse($bytes)
+
+    return [BitConverter]::ToUInt32($bytes, 0)
   }
-  process
+  catch
   {
-    try
-    {
-      if ($PSCmdlet.ParameterSetName -eq 'SubnetMask')
-      {
-        $PrefixLength = Convert-NetMaskToCIDR -SubnetMask $SubnetMask `
-          -ErrorAction Stop
-      }
-      else
-      {
-        $SubnetMask = Convert-CIDRToNetMask -PrefixLength $PrefixLength `
-          -ErrorAction Stop
-      }
-      if ($PSCmdlet.ParameterSetName -eq 'Hosts')
-      {
-        $PrefixLength = (Get-CidrFromHostCount -HostCount $HostCount).PrefixLength
-        $SubnetMask = Convert-CIDRToNetMask -PrefixLength $PrefixLength `
-          -ErrorAction Stop
-      }
-      $maxHosts = [math]::Pow(2, (32 - $PrefixLength)) - 2
-
-      $netMaskInt = ConvertIPv4ToInt -IPv4Address $SubnetMask
-      $ipInt = ConvertIPv4ToInt -IPv4Address $IPAddress
-
-      $networkID = ConvertIntToIPv4 -Integer ($netMaskInt -band $ipInt)
-
-      $broadcast = Add-IntToIPv4Address -IPv4Address $networkID `
-        -Integer ($maxHosts + 1)
-
-      $firstIP = Add-IntToIPv4Address -IPv4Address $networkID -Integer 1
-      $lastIP = Add-IntToIPv4Address -IPv4Address $broadcast -Integer (-1)
-
-      if ($PrefixLength -eq 32)
-      {
-        $broadcast = $networkID
-        $firstIP = $null
-        $lastIP = $null
-        $maxHosts = 0
-      }
-
-      $outputObject = New-Object -TypeName PSObject
-
-      $memberParam = @{
-        InputObject = $outputObject
-        MemberType  = 'NoteProperty'
-        Force       = $true
-      }
-      Add-Member @memberParam -Name CidrID -Value ('{0}/{1}' -f $networkID, $PrefixLength)
-      Add-Member @memberParam -Name NetworkID -Value $networkID
-      Add-Member @memberParam -Name SubnetMask -Value $SubnetMask
-      Add-Member @memberParam -Name PrefixLength -Value $PrefixLength
-      Add-Member @memberParam -Name HostCount -Value $maxHosts
-      Add-Member @memberParam -Name FirstHostIP -Value $firstIP
-      Add-Member @memberParam -Name LastHostIP -Value $lastIP
-      Add-Member @memberParam -Name Broadcast -Value $broadcast
-
-      Write-Output -InputObject $outputObject
-    }
-    catch
-    {
-      Write-Error -Exception $_.Exception `
-        -Category $_.CategoryInfo.Category
-    }
+    Write-Error -Exception $_.Exception `
+      -Category $_.CategoryInfo.Category
   }
-  end
+}
+
+function ConvertIntToIPv4
+{
+  [CmdletBinding()]
+  param(
+    [uint32]$Integer
+  )
+  try
   {
+    $bytes = [BitConverter]::GetBytes($Integer)
+    [Array]::Reverse($bytes)
+    ([IPAddress]($bytes)).ToString()
+  }
+  catch
+  {
+    Write-Error -Exception $_.Exception `
+      -Category $_.CategoryInfo.Category
   }
 }
 
